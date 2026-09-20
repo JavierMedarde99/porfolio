2ef79de2c2e9286fab3679ed63057960e505a57e
javimed <javiermedmata@gmail.com>
2026-09-20 14:13:48 +0200

    feat: TechBadge y ProjectCard reutilizables
    Closes #8

---
+++ src/lib/components/projects/TechBadge.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	interface Props {
+		technology: string;
+	}
+
+	let { technology }: Props = $props();
+</script>
+
+<span
+	class="rounded-full border border-zinc-300 px-3 py-1 text-xs font-medium transition-colors dark:border-zinc-700"
+>
+	{technology}
+</span>
