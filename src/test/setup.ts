64e4560e9c9a8cedc3e16af98375292d4802b9d6
javimed <javiermedmata@gmail.com>
2026-09-21 16:28:37 +0200

    fix: merge main + polyfill matchMedia para jsdom

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,16 @@
 import '@testing-library/jest-dom/vitest';
+
+// jsdom no implementa matchMedia: polyfill mínimo para componentes que lo usan
+Object.defineProperty(window, 'matchMedia', {
+	writable: true,
+	value: (query: string) => ({
+		matches: false,
+		media: query,
+		onchange: null,
+		addListener: () => {},
+		removeListener: () => {},
+		addEventListener: () => {},
+		removeEventListener: () => {},
+		dispatchEvent: () => false,
+	}),
+});
